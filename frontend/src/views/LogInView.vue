c4b42843262e2ba33d31fb2f9002097c3dea9c5d
Kirko87 <francescoragone87@gmail.com>
2022-05-19 12:49:48 +0200

    19-05

--- frontend/src/views/LogInView.vue
+++ frontend/src/views/LogInView.vue
@@ -1,71 +1,59 @@
 <template>
 
-<form class="LogInView" @submit="login">
-<label for="userName" class="LogInView_userName">User Name:<input required v-model="userName" type="text" placeholder="Votre nom d’utilisateur"/></label>
-<br>
-<label for="password" class="LogInView_password">Password:<input required v-model="password" type="password" placeholder=" Votre mot de passe"/></label>
-<button class="LogInView_button" type="submit">VALIDATE!</button>
-</form>
- 
+  <form class="LogInView" @submit="login">
+    <label for="userName" class="LogInView_userName">User Name:<input required v-model="userName" type="text"
+        placeholder="Votre nom d’utilisateur" /></label>
+    <br>
+    <label for="password" class="LogInView_password">Password:<input required v-model="password" type="password"
+        placeholder=" Votre mot de passe" /></label>
+    <div class="LogInView_button"><button  type="submit">VALIDATE!</button></div>
+  </form>
+
 </template>
 
 <script>
-export default{
-  data(){
+export default {
+  data() {
     return {
-      userName:null,
-      password:null
+      userName: null,
+      password: null
     }
   },
-  methods:{
-    login(event){
+  methods: {
+    login(event) {
       event.preventDefault()
-      fetch ("http://localhost:3010/Groupomania/login", {
-        method:"post",
+      fetch("http://localhost:3010/Groupomania/login", {
+        method: "post",
         headers: {
           Accept: "application/json",
           "Content-Type": "application/json",
         },
-        body:JSON.stringify({email:this.userName, password:this.password})
-    })},
+        body: JSON.stringify({ email: this.userName, password: this.password })
+      })
+    },
   }
 }
 
 </script>
 
 <style lang="scss">
+
 .LogInView {
 
-&_userName, &_password   {
-     color:rgb(65, 59, 59);
-    font-size:23px;
-    margin-bottom:5px;
-   text-shadow: 1px 1px 9px rgb(140, 140, 140);
-   
-}
+  &_userName, &_password {
+    display: flex;
+    justify-content: center;
+    color: rgb(65, 59, 59);
+    font-size: 23px;
+    margin-bottom: 5px;
+    text-shadow: 1px 1px 9px rgb(140, 140, 140);
+  }
 
-#userName, #password {
+  &_button {
     display: flex;
     align-items: center;
     justify-content: center;
-    }
-
-#logList {
-    display: flex;
-    flex-direction: column;  
-    margin: 30px
+    margin-top: 70px;
+ }
 }
-
-#buttonContainer {
-  display:flex;
-   justify-content: center;
-}
-
-#button_div {
-  margin:5px;
-  user-select:none;
-}
-}
-
-
 </style>
--- frontend/src/views/SignInView.vue
+++ frontend/src/views/SignInView.vue
@@ -1,62 +1,83 @@
 <template>
-<form id="signList">
-<label for="userName" class="userName">User Name:</label>
-<input type="text" id="userName"  placeholder="Insérer votre nom d’utilisateur"/>
-<br>
-<label for="name" class="name"> Name:</label>
-<input type="text" id="name" placeholder="Insérer votre prénom"/>
-<br>
-<label for="surame" class="surname">Surname:</label>
-<input type="text" id="surname" placeholder="Insérer votre nom de famille"/>
-<br>
-<label for="email" class="email">Email:</label>
-<input id="email" placeholder="Insérer votre email"/>
-<br>
-<label for="email" class="re-email"> Re-enter Email:</label>
-<input id="re-email" placeholder="Ressaisir votre email"/>
-<br>
-<label for="password" class="password">Password:</label>
-<input id="password" placeholder="Introduisez votre mot de passe"/>
-<br>
-<label for="password" class="re-password">Re-enter Password:</label>
-<input id="re-password" placeholder="Ressaisir votre mot de passe"/>
-</form>
- <div id="buttonContainer"><button id="button_div">VALIDATE!</button></div>
+
+  <form class="SignInView" @submit="signIn">
+    <label for="userName" class="SignInView_userName">User Name:<input required v-model="userName" type="text"
+     placeholder="Insérer votre nom d’utilisateur" /></label>
+    <br>
+    <label for="name" class="SignInView_name"> Name:<input required v-model="name" type="text"
+     placeholder="Insérer votre prénom" /></label> 
+    <br>
+    <label for="surame" class="SignInView_surname">Surname:<input required v-model="surname" type="text"
+     placeholder="Insérer votre nom de famille" /></label>
+    <br>
+    <label for="email" class="SignInView_email">Email:<input required v-model="email" type="email"
+    placeholder="Insérer votre email" /></label>
+    <br>
+    <label for="email" class="SignInView_re-email"> Re-enter Email:<input required v-model="reEnterEmail" type="email"
+      placeholder="Ressaisir votre email" /></label>
+    <br>
+    <label for="password" class="SignInView_password">Password:<input required v-model="password" type="password"
+    placeholder="Introduisez votre mot de passe" /></label>  
+    <br>
+    <label for="password" class="SignInView_re-password">Re-enter Password:<input required v-model="reEnterPassword" type="password"
+    placeholder="Ressaisir votre mot de passe" /></label>  
+
+    <div class="SignInView_button"><button  type="submit">VALIDATE!</button></div>
+  </form>
+
 
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      userName: null,
+      name: null,
+      surname: null,
+      email: null,
+      reEnterEmail: null,
+      password: null,
+      reEnterPassword: null,
+    }
+  },
+  methods: {
+    signIn(event) {
+      event.preventDefault()
+      fetch("http://localhost:3010/Groupomania/signup", {
+        method: "post",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ userName: this.userName, name:this.name, surname:this.surname, email:this.email, reEnterEmail:this.reEnterEmail, password: this.password, reEnterPassword: this.reEnterPassword})
+      })
+    },
+  }
+}
 
-<style>
+</script>
 
+<style lang="scss">
 
-#userName, #name, #surname, #email, #re-email, #password, #re-password {
+.SignInView {
+
+ &_userName, &_name, &_surname, &_email,
+  &_re-email, &_password, &_re-password {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+ color: rgb(65, 59, 59);
+  font-size: 23px;
+  margin-bottom: 5px;
+  text-shadow: 1px 1px 9px rgb(140, 140, 140);
+}
+
+  &_button {
     display: flex;
     align-items: center;
     justify-content: center;
+    margin-top: 70px;
+ }
 }
-
-.userName, .name, .surname, .email, .re-email, .password, .re-password {
-    color:rgb(65, 59, 59);
-    font-size:23px;
-    margin-bottom:5px;
-   text-shadow: 1px 1px 9px rgb(140, 140, 140);
-   
-   
-}
-
-#signList{
-    display: flex;
-    flex-direction:column;
-    margin: 30px
-}
-  
-#buttonContainer {
-  display:flex;
-   justify-content: center;
-}
-
-#button_div {
-  margin:5px;
-  user-select:none;
-} 
 </style>
